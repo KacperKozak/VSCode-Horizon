ffdb6428ed973aaf9b958cda219c0b750624c92b
Kacper Kozak <kacperkk@gmail.com>
2023-08-03 21:26:50 +0200

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,44 @@
 import * as vscode from 'vscode'
 
+type Brackets = [string, string]
+
+/*
+Works for
+	[ test, test2 ]
+	{ test1: 'a', test2: 2, test }
+	[ 321, 432, 543 ]
+	
+Mostly works for (treats element name as one of elements in array)
+	<div key={i} color={color} data-test={key}>
+	
+Don't work (TODO)
+	(key: Key, value: number) => {}
+	if (some && condition || here)
+	type Union = 'a' | 'b' | 'c'
+	{ obj: { a, b }, arr: [x, y] }
+	[ [123, 123], [321, 321] ]
+*/
+
 interface BracketType {
-    bracket: RegExp
+    bracket: Brackets
     separator: string
     joiner: string
     word: RegExp | undefined
 }
 
 const types: BracketType[] = [
-    { bracket: /<.*>/, separator: ' ', joiner: ' ', word: /[^<>\s]+/ },
-    { bracket: /{.*}/, separator: ',', joiner: ', ', word: /[^{}\,]+/ },
-    { bracket: /\[.*\]/, separator: ',', joiner: ', ', word: /[^[\]\,]+/ },
-    { bracket: /\(.*\)/, separator: ',', joiner: ', ', word: /[^()\,]+/ },
+    { bracket: ['<', '>'], separator: ' ', joiner: ' ', word: /[^<>\s]+/ },
+    { bracket: ['{', '}'], separator: ',', joiner: ', ', word: /[^{}\,]+/ },
+    { bracket: ['[', ']'], separator: ',', joiner: ', ', word: /[^[\]\,]+/ },
+    { bracket: ['(', ')'], separator: ',', joiner: ', ', word: /[^()\,]+/ },
 ]
 
 export function activate(context: vscode.ExtensionContext) {
-    const moveRight = vscode.commands.registerCommand('react-organize.move-right', () => {
-        moveWord(1)
+    const moveRight = vscode.commands.registerCommand('horizon.move-right', () => {
+        moveWord(+1)
     })
 
-    const moveLeft = vscode.commands.registerCommand('react-organize.move-left', () => {
+    const moveLeft = vscode.commands.registerCommand('horizon.move-left', () => {
         moveWord(-1)
     })
 
@@ -35,12 +54,21 @@ const moveWord = (dir = 1) => {
 
     const cursorAnchor = editor.selection.anchor
 
-    // get everything that is inside { and } brackets
+    // get everything that is inside brackets
     let bracketRange: vscode.Range | undefined
     let bracketType: BracketType | undefined
 
     for (const type of types) {
-        const range = editor.document.getWordRangeAtPosition(cursorAnchor, type.bracket)
+        const currentLine = editor.document.lineAt(cursorAnchor.line).text
+
+        // console.log(type.bracket.join(''), cursorAnchor.character, '----')
+        // TODO
+        splitByBracket(currentLine, type.bracket, type.separator, cursorAnchor.character)
+
+        const range = editor.document.getWordRangeAtPosition(
+            cursorAnchor,
+            new RegExp(`\\${type.bracket[0]}.+?\\${type.bracket[1]}`),
+        )
         if (range) {
             bracketRange = range
             bracketType = type
@@ -66,6 +94,10 @@ const moveWord = (dir = 1) => {
     const bracketTextArray = bracketTextWithoutBrackets
         .split(bracketType.separator)
         .map((item) => item.trim())
+
+    // console.log(cursorWord)
+    // console.log(bracketText)
+    // console.log(bracketTextArray)
 
     // move cursorWord one position to the right
     const cursorWordIndex = bracketTextArray.indexOf(cursorWord)
@@ -113,4 +145,103 @@ const moveWord = (dir = 1) => {
                 ),
             )
         })
+}
+
+// split by bracket including nested brackets and cursor position
+const splitByBracket = (
+    text: string,
+    [l, r]: Brackets,
+    separator: string,
+    cursorPosition: number,
+) => {
+    // find first bracket left of cursor
+    let pos = cursorPosition
+    let leftBracketPos = -1
+    while (text[pos]) {
+        if (text[pos] === l) {
+            leftBracketPos = pos
+            break
+        }
+        pos--
+    }
+
+    // find first bracket right of cursor including nested brackets
+    pos = cursorPosition
+    let rightBracketPos = -1
+    let nestedBrackets = 0
+    while (text[pos]) {
+        if (text[pos] === l) {
+            nestedBrackets++
+        } else if (text[pos] === r) {
+            if (nestedBrackets === 0) {
+                rightBracketPos = pos
+                break
+            }
+            nestedBrackets--
+        }
+        pos++
+    }
+
+    // find left current item position by the cursor
+    let leftCurrentPos = cursorPosition
+    while (text[leftCurrentPos - 1]) {
+        if (text[leftCurrentPos - 1] === separator) {
+            break
+        }
+        if (text[leftCurrentPos - 1] === l) {
+            break
+        }
+        leftCurrentPos--
+    }
+
+    // find left current item position by the cursor including nested brackets
+    let rightCurrentPos = cursorPosition
+    let currentPosNested = 0
+    while (text[rightCurrentPos]) {
+        if (text[rightCurrentPos + 1] === separator) {
+            break
+        }
+        if (text[rightCurrentPos + 1] === r) {
+            break
+        }
+        if (text[rightCurrentPos + 1] === l) {
+            currentPosNested++
+        } else if (text[rightCurrentPos + 1] === r) {
+            if (currentPosNested === 0) {
+                break
+            }
+            currentPosNested--
+        }
+        rightCurrentPos++
+    }
+
+    const currentItemText = text.slice(leftCurrentPos, rightCurrentPos + 1)
+
+    const bracketText = text.slice(leftBracketPos + 1, rightBracketPos)
+
+    const result: string[] = []
+
+    // split by separator including nested brackets
+    let current = ''
+    let nested = 0
+    for (const char of bracketText) {
+        if (char === l) {
+            nested++
+        } else if (char === r) {
+            nested--
+        } else if (char === separator && nested === 0) {
+            result.push(current)
+            current = ''
+            continue
+        }
+        current += char
+    }
+    result.push(current)
+
+    const trimmed = result.map((item) => item.trim())
+
+    // console.log(l, r, '---------')
+    // console.log(bracketText)
+    // console.log(trimmed)
+    // console.log(currentItemText)
 }
